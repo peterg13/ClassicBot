7b10587840b3066fbd4075a1505555a14660d833
Peter Gustafson <petergustafson19@gmail.com>
2019-08-22 23:22:06 -0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,8 +2,9 @@
 
 //the file that contains the bot's Discord token (this file is in gitignore)
 var discordAuth = require("./discordAuth.json");
-//the file that contains the bots Blizzard ID and Secret
-//var blizzardAuth = require("./blizzardAuth.json");
+
+//my character class
+let Character = require('./character.js');
 
 const fs = require('fs');
 
@@ -29,6 +30,11 @@ client.on('message', message => {
           addCharacter(splitMessage[1], splitMessage[2]);
         }
         break;
+
+      //prints the local database into Discord
+      case "!race":
+        printLocalDatabase(message);
+        break;
     }
 });
 
@@ -49,7 +55,31 @@ function addCharacterConsoleMessage(newJSON){
   console.log("----------------------------------------------------");
   console.log(newJSON);
   console.log("----------------------------------------------------\n");
-}
+};
+
+//prints a readable version of the local database
+function printLocalDatabase(message){
+  var database = JSON.parse(fs.readFileSync("./local_database.json"));
+  var databaseCharacters = [];
+  for(let i = 0; i < database.Characters.length; i++){
+    databaseCharacters.push(new Character(database.Characters[i]._name, database.Characters[i]._realm, database.Characters[i]._level, database.Characters[i]._race, database.Characters[i]._class));
+  }
+  //databaseCharacters should now be an array of Character classes
+  //lets sort the array in descending order based on level
+  databaseCharacters.sort(function(a, b){return b.getLevel()-a.getLevel()});
+  var databaseString = generateDiscordFriendlyDatabaseString(databaseCharacters, database.LastUpdated);
+  message.channel.send(databaseString);
+};
+
+//function to get a string that displays the current race in an easy to read form
+function generateDiscordFriendlyDatabaseString(characterArray, lastUpdated){
+  var completedString = "```";
+  for(let i = 0; i < characterArray.length; i++){
+    completedString = completedString + (i+1) + ". (" + characterArray[i].getLevel() + ") " + characterArray[i].getName() + "\n";
+  }
+  completedString = completedString + "```\n"
+  return completedString + "*" + lastUpdated + "*";
+};
 
 //logs the client in
 client.login(discordAuth.token);
