6e139cc3f2bd92fc68b0f549db7050e13fdadb7e
Peter Gustafson <petergustafson19@gmail.com>
2019-08-22 21:48:51 -0700

    added chartacter checking for valid characters

--- server_manager.js
+++ server_manager.js
@@ -13,7 +13,7 @@ var armory = new Blizzard(blizzardAuth.ID, blizzardAuth.Secret);
 //-------------          The program itself          -------------
 //----------------------------------------------------------------
 
-//initilizes out connection with Firebase
+//initilizes our connection with Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://world-of-warcraft-51327.firebaseio.com"
@@ -30,31 +30,31 @@ let db = admin.firestore();
 //if so then update the database
 //write to file for discord bot
 getNewCharacters().then(newCharacters => {
-  console.log(newCharacters);
+  //console.log(newCharacters);
   pullDatabase().then(database =>{
     var addedCharacters = [];
     newCharacters.forEach(newCharacter => {
-      if(!doesCharacterExist(newCharacter, database)){
+      if(!doesCharacterExist(newCharacter, database) && isValidCharacter(newCharacter)){
         //adds the character to online database
-        //writeCharacterToFirebase(newCharacter);
+        writeCharacterToFirebase(newCharacter);
         //adds character to our locally copied database for further evaluation
         addedCharacters.push(newCharacter);
       }
     })
     
     updateCharacters(database).then(promises => {
-      //database now contains all the characters new and old
       addedCharacters.forEach(addedCharacter =>{
         database.push(addedCharacter);
       });
+      //database now contains all the characters new and old
       writeDatabaseToFile(database);
       //console.log(database);
     });
     //database now contains all the characters new and old
-    //addedCharacters.forEach(addedCharacter =>{
-      //database.push(addedCharacter);
-    //});
-    //writeDatabaseToFile(database);
+    addedCharacters.forEach(addedCharacter =>{
+      database.push(addedCharacter);
+    });
+    writeDatabaseToFile(database);
     //console.log(database);
   });
 });
@@ -107,33 +107,43 @@ function pullDatabase(){
 };
 
 function doesCharacterExist(checkCharacter, characterArray){
-  for(let i = 0; i < characterArray.length; i++){
-    var characterInArray = characterArray[i];
-    if(characterInArray.getName().toLowerCase() === checkCharacter.getName().toLowerCase() && 
-    characterInArray.getRealm().toLowerCase() === checkCharacter.getRealm().toLowerCase()){
-      return true;
+  if(isValidCharacter(checkCharacter)){
+    for(let i = 0; i < characterArray.length; i++){
+      var characterInArray = characterArray[i];
+      if(characterInArray.getName().toLowerCase() === checkCharacter.getName().toLowerCase() && 
+      characterInArray.getRealm().toLowerCase() === checkCharacter.getRealm().toLowerCase()){
+        return true;
+      }
     }
   }
   return false;
 };
 
 function updateCharacters(database){ 
+  //gets the latest armory update for all characters in the online database and saves it in new array
   var updatedCharactersPromises = database.map(dbCharacter => {
     //clone the character so we don't update the database itself
     var characterCopy = dbCharacter.clone();
     return armory.requestCharacter(characterCopy);
   });
 
+  //compares the new armory pull to what was in the database.  If there is an update it will push the update to Firebase
   return Promise.all(updatedCharactersPromises).then(updatedCharacters => {  
     for(let i = 0; i < updatedCharacters.length; i++){
-      for(let j = 0; j < database.length; j++){
-        //first let's make sure we are comparing the correct characters.  if not, moves on to next character in database and checks again
-        if(updatedCharacters[i].getName().toLowerCase() === database[j].getName().toLowerCase() &&
-        updatedCharacters[i].getRealm().toLowerCase() === database[j].getRealm().toLowerCase()){
-          //now we have to check if the level is different (no race changes exist in classic)
-          if(updatedCharacters[i].getLevel() != database[j].getLevel()){
-            //overwrites the database entry for that character
-            //writeCharacterToFirebase(updatedCharacters[i]);
+      //first check is it is a valid character (in case something went wrong with the armory pull)
+      if(isValidCharacter(updatedCharacters[i])){
+        for(let j = 0; j < database.length; j++){
+          //let's make sure we are comparing the correct characters.  if not, moves on to next character in database and checks again
+          if(updatedCharacters[i].getName().toLowerCase() === database[j].getName().toLowerCase() &&
+          updatedCharacters[i].getRealm().toLowerCase() === database[j].getRealm().toLowerCase()){
+            //now we have to check if the level is different (no race changes exist in classic)
+            if(updatedCharacters[i].getLevel() != database[j].getLevel()){
+              //overwrites the database entry for that character
+              console.log("Updated " + updatedCharacters[i].getName() + " level from " + database[j].getLevel() + " to " + updatedCharacters[i].getLevel());
+              database[j] = updatedCharacters[i];
+              writeCharacterToFirebase(updatedCharacters[i]);
+              
+            }
           }
         }
       }
@@ -185,6 +195,7 @@ function isValidCharacter(character){
     character.getLevel() === undefined ||
     character.getRace() === undefined ||
     character.getClass() === undefined){
+    console.log(character.getName() + " : " + character.getRealm() + " is not a valid character");
     return false;
   }
   else {
